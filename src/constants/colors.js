745e6ca51c005b583a512c6f6917e688e4948f5c
Talabi-David-Adeoluwa2010 <talabidavid275@gmail.com>
2026-08-03 14:17:32 +0100

    Create colors.js

---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+export const BASE_COLORS = {
+  obsidian: '#1C1B20',
+  obsidianLight: '#2A2930',
+  parchment: '#F7F4EB',
+  parchmentWarm: '#F3E5AB',
+  terracotta: '#C86D51',
+  sageGreen: '#8A9A86',
+  gold: '#D4AF37',
+  textDark: '#1C1B20',
+  textMuted: '#9E9A90',
+  highlightYellow: 'rgba(242, 201, 76, 0.45)',
+  tintOverlay: 'rgba(255, 160, 0, 0.15)',
+};
